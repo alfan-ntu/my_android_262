6fca4fe79148ff1eb5229515834a51ab0dc5d550
Al Fan <alfan@luminoncore.com>
2015-12-13 23:36:34 +0800

    Added an editText and a button and an on-click method 'submit' to toast a message on screen

--- SimpleUI/app/src/main/java/com/example/lcadmin/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/lcadmin/simpleui/MainActivity.java
@@ -8,24 +8,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        editText = (EditText)findViewById(R.id.inputText);
+        editText.setText("1234");
+
+    }
+
+    public void submit(View view){
+        String text = editText.getText().toString();
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 
     @Override
