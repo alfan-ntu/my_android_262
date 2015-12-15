769fce4543c1e7ba11c97b567260d19bb2c6b9a1
Al Fan <alfan@luminoncore.com>
2015-12-16 01:12:39 +0800

    added java activity for DrinkMenuActivity

---
+++ SimpleUI/app/src/main/java/com/example/lcadmin/simpleui/DrinkMenuActivity.java
@@ -0,0 +1,23 @@
+package com.example.lcadmin.simpleui;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by lcadmin on 2015/12/16.
+ * added a new activity class "DrinkMenuActivity"
+ */
+public class DrinkMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+/*
+    applies layout resource
+ */
+        setContentView(R.layout.activity_drink_menu);
+
+    }
+}
